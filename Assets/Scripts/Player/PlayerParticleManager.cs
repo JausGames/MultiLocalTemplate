2ee2cf52c2993402c1d88fb770300e08a9db1d89
JaussGameDev <jaussgamedev@gmail.com>
2020-12-13 23:54:07 +0100

    remove some stuff

--- Assets/Scripts/Player/PlayerParticleManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-abstract public class PlayerParticleManager : MonoBehaviour
-{
-    abstract public void PlayHealthParticle();
-
-}
